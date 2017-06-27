53147482f97c8e174f668005a8bac8832b642403
李林 <ww1095@163.com>
2017-06-27 15:59:04 +0800

    init

---
+++ src/com/andli/uitest/UITestActivity.java
@@ -0,0 +1,16 @@
+package com.andli.uitest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class UITestActivity extends Activity {
+
+	LinearLayout linearLayout;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+	}
+}
